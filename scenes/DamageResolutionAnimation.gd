c22d5c1cb74ca9d9d7e1d00d588ab0f9fa17a89a
Simas V <simonas.vitkunas@gmail.com>
2023-12-14 00:58:40 +0200

    damage animation script

--- scenes/DamageResolutionAnimation.gd
+++ scenes/DamageResolutionAnimation.gd
@@ -3,3 +3,111 @@ class_name DamageResolutionAnimation
 
 @export var leftSide: ResolutionParamAnimator
 @export var rightSide: ResolutionParamAnimator
+
+@export var shieldsDelay: float
+@export var smashAnimTime: float
+@export var rightSideAttackChip: ResourceTracker
+@export var leftSideAttackChip: ResourceTracker
+
+var attackComplete = false:
+    set(new):
+        if new and defenceComplete:
+            attackAndShieldBothComplete.emit()
+        attackComplete = new
+
+var delayShieldComplete = false
+var defenceComplete = false:
+        set(new):
+            if new and attackComplete:
+                attackAndShieldBothComplete.emit()
+                defenceComplete = new
+
+signal shieldGo
+signal done
+signal attackAndShieldBothComplete
+
+func setLeftParam(param: CardParam.ParamType):
+    leftSide.paramToShow = param
+
+func setRightParam(param: CardParam.ParamType):
+    rightSide.paramToShow = param
+
+func start():
+
+    var sideWithAttack = leftSide if leftSide.paramToShow == CardParam.ParamType.ATTACK else rightSide
+    var sideWithDefence = rightSide if rightSide.paramToShow == CardParam.ParamType.DEFENCE else leftSide
+
+    var totalAttackAvailable = sideWithAttack.containerToQuery.getTotalAttack()
+    var totalDefenceAvailable = sideWithDefence.containerToQuery.getTotalDefence()
+    var anyAttackAvailable = totalAttackAvailable > 0
+    var anyDefenceAvailable = totalDefenceAvailable > 0
+
+    if anyAttackAvailable:
+        sideWithAttack.start()
+        await sideWithAttack.done
+        markAttackComplete()
+    else:
+        markAttackComplete()
+
+    if anyDefenceAvailable:
+        if attackComplete:
+            sideWithDefence.start()
+        else:
+            await shieldGo
+            sideWithDefence.start()
+    else:
+        defenceComplete = true
+
+    if not (attackComplete and defenceComplete):
+        await attackAndShieldBothComplete
+    
+    if not anyAttackAvailable:
+       pass
+
+    elif not anyDefenceAvailable:
+        sideWithAttack.hit()
+        await sideWithAttack.hitDone        
+    
+    elif totalDefenceAvailable >= totalAttackAvailable:
+        sideWithAttack.smash()
+        sideWithDefence.smash()
+        await sideWithAttack.smashDone
+        sideWithAttack.disappear()
+        sideWithDefence.setNewTarget(totalDefenceAvailable - totalAttackAvailable, 0.1)
+        await sideWithDefence.smashReturnDone       
+
+    elif totalAttackAvailable > totalDefenceAvailable:
+        sideWithAttack.smash()
+        sideWithDefence.smash()
+        await sideWithAttack.smashDone
+        sideWithDefence.disappear()
+        sideWithAttack.setNewTarget(totalAttackAvailable - totalDefenceAvailable, 0.1)
+        await sideWithAttack.smashReturnDone
+        sideWithAttack.hit()
+        await sideWithAttack.hitDone
+
+    cleanup()
+    done.emit()
+
+func cleanup():
+    leftSide.disappear()
+    rightSide.disappear()
+    attackComplete = false
+    defenceComplete = false
+    delayShieldComplete = false
+
+func markAttackComplete():
+    attackComplete = true
+    shieldGo.emit()
+
+func markDelayShieldComplete():
+    delayShieldComplete = true
+
+func startShieldDelayTimer():
+    await get_tree().create_timer(shieldsDelay).timeout
+    shieldGo.emit()
+
+
+
+
+
--- scenes/DamageShower.gd
+++ scenes/DamageShower.gd
@@ -4,6 +4,7 @@ class_name ResolutionParamAnimator
 @export var containerToQuery: CardContainer
 @export var cardNudgeVector: Vector2
 @export var paramToShow: CardParam.ParamType
+@export var animation: AnimationPlayer
 
 @export var maxScale: float
 @export var valueAtMaxScale: int
@@ -14,18 +15,32 @@ class_name ResolutionParamAnimator
 @export var iconSprite: Sprite2D
 @export var attackTex: Texture2D
 @export var defenceTex: Texture2D
+@export var disappearTime: float
 
 signal done
+signal hitDone
+signal smashDone
+signal smashReturnDone
+
+var textAndScaleAnimationTime: float = 0.1
 
 var currentValue:float = 0:
 	set(newValue):
 		valueLabel.text = str(newValue)
 		currentValue = newValue
-var targetValue:float = 0
+
+var targetValue:float = 0:
+	set(newValue):
+		var weight = targetValue/valueAtMaxScale
+		var lerpedValue = lerp(startScale, maxScale, weight)
+		targetScale = clamp(lerpedValue, startScale, maxScale)
+		targetValue = newValue
+
 var valueChangeCounter = 0
 var targetScale = startScale
 
 func start():
+	animation.play("RESET")
 	match paramToShow:
 		CardParam.ParamType.ATTACK:
 			iconSprite.texture = attackTex
@@ -49,12 +64,8 @@ func addParamFromOneCard(card: CardData):
 			additionalParamValue = card.getDefence()
 
 	if additionalParamValue > 0:
-		targetValue += additionalParamValue
-		var weight = targetValue/valueAtMaxScale
-		var lerpedValue = lerp(startScale, maxScale, targetValue/valueAtMaxScale)
-		targetScale = clamp(lerpedValue, startScale, maxScale)
-		print('target scale', targetScale, ' lerpedValue ', lerpedValue, ' weight ', weight)
-		valueChangeCounter = 0
+		setNewTarget(targetValue+additionalParamValue, singleCardAnimationTime)
+		#print('target scale', targetScale, ' lerpedValue ', lerpedValue, ' weight ', weight)
 		var tween = get_tree().create_tween()
 		var cd = CardDisplayLord.getCardDisplay(card)
 		
@@ -70,6 +81,25 @@ func _process(delta):
 		var clampedScale = clamp(lerp(scale.x, targetScale, valueChangeCounter/singleCardAnimationTime), scale.x, targetScale)
 		scale = Vector2(clampedScale, clampedScale)
 
+func setNewTarget(newTarget, time: float = textAndScaleAnimationTime):
+	targetValue = float(newTarget)
+	textAndScaleAnimationTime = time
+	valueChangeCounter = 0
 
+func smash():
+	animation.play("smash")
+	await animation.animation_finished
+	smashDone.emit()
+	animation.play("smashReturn")
+	smashReturnDone.emit()
 
+func hit():
+	animation.play("hit")
+	await animation.animation_finished
+	hitDone.emit()
 
+func disappear():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(0.05,0.05), disappearTime)
+	await tween.finished
+	visible = false
